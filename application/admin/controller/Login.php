937aa3d574478de0c59bb6bdc47706ab7c755a01
温建平 <1658159330@qq.com>
2018-01-13 21:28:28 +0800

    修改登录功能，登录成功记录登录ip和登录时间、登录次数

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -9,13 +9,14 @@ class Login extends Controller
     public function index(){
         if(request()->isAjax()){
             $result=array('status'=>0,'msg'=>'','url'=>'');
-            if(captcha_check(input('code'))){//验证码通过返回true
+            $data=input('post.');//获取post提交的信息
+            if(!captcha_check($data['code'])){//验证码通过返回true
                 $result['msg']='验证码错误,请重新输入';
                 echo json_encode($result);
                 exit();
             }
             $user=new userModel;
-            $res=$user->checkLogin( input('post.'));   
+            $res=$user->checkLogin($data);   
             echo json_encode($res);
             exit();
         }
--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -2,20 +2,35 @@
 
 namespace app\admin\model;
 use think\Model;
+use think\Request;
 
 class User extends Model{
 	public function checkLogin($data){
         $result=array('status'=>0,'msg'=>'');
         $msg='';
-        $res= User::getByLoginName($data['username']);
+        $res=User::query("SELECT * FROM jld_user where login_name='".$data['username']."' AND isnull(is_delete,0)=0");
         if($res){
-            if( md5($data['password'])==$res['login_passwd']){
-                if($res['user_state']!=1){
-                    $msg='此账号已冻结，请联系管理员';
+            if( md5($data['password'])==$res[0]['login_passwd']){
+                if($res[0]['user_state']!=1){
+                    $msg='此账号已冻结审核状态，请联系管理员';
                 }else{
-                    session('id', $res['id']);
-                    session('login_name', $res['login_name']);
-                    session('user_name', $res['user_name']);
+                	// 启动事务
+                	$updateData=array();
+                	$updateData['login_ip']=Request::instance()->ip();
+                	$updateData['login_sum']=$res[0]['login_sum']==''?1:((int)$res[0]['login_sum']+1);
+                	$updateData['login_time']=date('Y-m-d H:i:s');
+					User::startTrans();
+					try{
+					    User::where('user_id', $res[0]['user_id'])->update($updateData);
+					    // 提交事务
+					    User::commit();    
+					} catch (\Exception $e) {
+					    // 回滚事务
+					    User::rollback();
+					}
+                    session('user_id', $res[0]['user_id']);
+                    session('login_name', $res[0]['login_name']);
+                    session('user_name', $res[0]['user_name']);
                     $result['status']=1;
                     $msg='登录成功,正在跳转到系统首页...';
                     $result['url']= request()->root(true).'/admin/index/index';
