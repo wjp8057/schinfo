2bb5677d53c5c7eff375851076321dd8b51cf93a
温建平 <1658159330@qq.com>
2018-01-13 21:20:41 +0800

    判断用户表id修改成判断用户id

--- application/common/controller/Common.php
+++ application/common/controller/Common.php
@@ -7,7 +7,7 @@ class Common extends Controller
 {
     public function _initialize()
     {
-        if(!session('login_name')&&!session('id')&&!session('user_name')){
+        if(!session('login_name')&&!session('user_id')&&!session('user_name')){
             $this->error('您尚未登录系统，请先去登录...','admin/login/index');
         }
     }
