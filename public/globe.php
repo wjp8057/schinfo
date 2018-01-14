095652e08a19fa4ad10989a26409f6623053bd1e
温建平 <1658159330@qq.com>
2018-01-14 22:22:32 +0800

    跳整css文件位置，可以让自定义菜单图标显示

--- public/globe.php
+++ public/globe.php
@@ -1,7 +1,7 @@
 <link rel="SHORTCUT ICON" href="IMG/syslogo.ico"/>
 <link href="EASYUI/themes/default/easyui.css" rel="stylesheet" type="text/css"/>
-<link href="EASYUI/themes/icon.css" rel="stylesheet" type="text/css"/>
 <link href="EASYUI/themes/bootstrap/easyui.css"  rel="stylesheet" type="text/css">
+<link href="EASYUI/themes/icon.css" rel="stylesheet" type="text/css"/>
 <script src="EASYUI/jquery.min.js"  type="text/javascript"></script>
 <script src="EASYUI/jquery.easyui.min.js"  type="text/javascript"></script>
 <script src="JS/checkfunc.js" type="text/javascript"></script>
